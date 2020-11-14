92263acffd558d4b5f7a3bfd4c99c79729820769
Mark Colby <macolby14@gmail.com>
2020-11-13 23:08:04 -1000

    fixup typing

--- config/keys.ts
+++ config/keys.ts
@@ -1,11 +1,11 @@
 // ADD YOUR OWN KEYS AND RENAME THIS FILE TO keys.js
 const GOOGLE_TOKENS = {
-  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
 };
 
 const SESSION = {
-  COOKIE_KEY: process.env.COOKIE_KEY,
+  COOKIE_KEY: process.env.COOKIE_KEY as string,
 };
 
 const KEYS = {
