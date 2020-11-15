924a2882c0ffeab21df7d0dce9b786e4b5a4d635
Mark Colby <macolby14@gmail.com>
2020-11-15 12:07:43 -1000

    ormconfig for both ts and js

---
+++ build/ormconfig.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    synchronize: true,
+    logging: true,
+    entities: process.env.NODE_ENV === 'production' ? ['build/entity/*.js'] : ['./entity/*.ts'],
+};
+//# sourceMappingURL=ormconfig.js.map
